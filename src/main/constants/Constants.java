75d32a79705da8cee43a03643deea35fd0adbff8
Pr1 <karpovicvlad042@gmail.com>
2025-01-22 11:55:43 +0300

    Init commit

--- src/main/constants/Constants.java
+++ src/main/constants/Constants.java
@@ -1,8 +1,8 @@
 package main.constants;
 
 public class Constants {
-    public static final String LOG_FILE = "C:\\Users\\user\\IdeaProjects\\TeachMeSkills_C32_HW_Lesson_25\\src\\main\\resources\\log.txt"; // Лог-файл в корневой папке
-    public static final String USER_DATA_FILE = "C:\\Users\\user\\IdeaProjects\\TeachMeSkills_C32_HW_Lesson_25\\src\\main\\resources\\users.txt"; // Файл с данными пользователей
+    public static final String LOG_FILE = "C:\\Users\\user\\IdeaProjects\\TeachMeSkills_C32_HW_Lesson_25\\src\\main\\resources\\log.txt";
+    public static final String USER_DATA_FILE = "C:\\Users\\user\\IdeaProjects\\TeachMeSkills_C32_HW_Lesson_25\\src\\main\\resources\\users.txt";
     public static final String SESSION_USER = "user";
     public static final String SESSION_ROLE = "role";
 }
